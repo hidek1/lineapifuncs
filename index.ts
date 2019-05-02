fb88faf4d9056e8e8b40751f08d239fac4d5195e
秀和 <hidekazu5200@gmail.com>
2019-05-02 15:47:21 +0900

    動画、音声、ファイル、位置情報、スタンプの送信を実装

--- index.ts
+++ index.ts
@@ -30,7 +30,7 @@ app.post('/callback', line.middleware(config), (req, res) => {
  * @return {Promise} 
  */
 
-
+// replyMessage
 // replyTokenは一定の期間が経過すると無効になるのでreplymessageは直後に送る用に使う
 function replyTextMessage(event: any, text: string){
   const echoMessage = {
@@ -50,6 +50,46 @@ function replyImageMessage(event: any, originalContentUrl: string, previewImageU
   return client.replyMessage(event.replyToken, echoMessage);
 };
 
+function replyVideoMessage(event: any, originalContentUrl: string, previewImageUrl: string){
+  const echoMessage = {
+    type: "video",
+    originalContentUrl: originalContentUrl,
+    previewImageUrl: previewImageUrl
+  };
+  return client.replyMessage(event.replyToken, echoMessage);
+};
+
+function replyAudioMessage(event: any, originalContentUrl: string, duration: number){
+  const echoMessage = {
+    type: "audio",
+    originalContentUrl: originalContentUrl,
+    duration: duration
+  };
+  return client.replyMessage(event.replyToken, echoMessage);
+};
+
+function replyLocationMessage(event: any, title: string, address: string, latitude: number, longitude: number){
+  const echoMessage = {
+    type: "location",
+    title: title,
+    address: address,
+    latitude: latitude,
+    longitude: longitude
+  };
+  return client.replyMessage(event.replyToken, echoMessage);
+};
+
+// idについてはhttps://developers.line.biz/media/messaging-api/sticker_list.pdfを参照
+function replyStickerMessage(event: any, packageId: number, stickerId: number){
+  const echoMessage = {
+    type: "sticker",
+    packageId: packageId,
+    stickerId: stickerId
+  };
+  return client.replyMessage(event.replyToken, echoMessage);
+};
+
+// pushMessage
 //任意のタイミングでメッセージが遅れるやつ
 function pushTextMessage(user_or_group_or_room_id : string, text :string){
   const echoMessage = {
@@ -59,22 +99,118 @@ function pushTextMessage(user_or_group_or_room_id : string, text :string){
   return client.pushMessage(user_or_group_or_room_id,echoMessage);
 }
 
-function pushImageMessage(user_or_group_or_room_id : string, text :string){
+//jpgしか送れないです
+function pushImageMessage(user_or_group_or_room_id : string, originalContentUrl: string, previewImageUrl: string){
   const echoMessage = {
-    type: 'text',
-    text: text
+    type: "image",
+    originalContentUrl: originalContentUrl,
+    previewImageUrl: previewImageUrl
   };
   return client.pushMessage(user_or_group_or_room_id,echoMessage);
 }
 
+function pushVideoMessage(user_or_group_or_room_id : string, originalContentUrl: string, previewImageUrl: string){
+  const echoMessage = {
+    type: "video",
+    originalContentUrl: originalContentUrl,
+    previewImageUrl: previewImageUrl
+  };
+  return client.pushMessage(user_or_group_or_room_id,echoMessage);
+}
+
+function pushAudioMessage(user_or_group_or_room_id : string, originalContentUrl: string, duration: number){
+  const echoMessage = {
+    type: "audio",
+    originalContentUrl: originalContentUrl,
+    duration: duration
+  };
+  return client.pushMessage(user_or_group_or_room_id,echoMessage);
+}
+
+function pushLocationMessage(user_or_group_or_room_id: string, title: string, address: string, latitude: number, longitude: number){
+  const echoMessage = {
+    type: "location",
+    title: title,
+    address: address,
+    latitude: latitude,
+    longitude: longitude
+  };
+  return client.pushMessage(user_or_group_or_room_id, echoMessage);
+};
+
+// idについてはhttps://developers.line.biz/media/messaging-api/sticker_list.pdfを参照
+function pushStickerMessage(user_or_group_or_room_id: string, packageId: number, stickerId: number){
+  const echoMessage = {
+    type: "sticker",
+    packageId: packageId,
+    stickerId: stickerId
+  };
+  return client.pushMessage(user_or_group_or_room_id, echoMessage);
+};
+
+
+
+// multicastMessage
 //任意のタイミングでメッセージが遅れるやつ(to 複数人)
-function multicastMessage(user_id_array: string[], text: string){
+function multicastTextMessage(user_id_array: string[], text: string){
   const echoMessage = {
     type: 'text',
     text: text
   };
   return client.multicast(user_id_array,echoMessage);
 }
+
+//jpgしか送れないです
+function multicastImageMessage(user_id_array: string[], originalContentUrl: string, previewImageUrl: string){
+  const echoMessage = {
+    type: "image",
+    originalContentUrl: originalContentUrl,
+    previewImageUrl: previewImageUrl
+  };
+  return client.multicast(user_id_array,echoMessage);
+}
+
+function multicastVideoMessage(user_id_array: string[], originalContentUrl: string, previewImageUrl: string){
+  const echoMessage = {
+    type: "video",
+    originalContentUrl: originalContentUrl,
+    previewImageUrl: previewImageUrl
+  };
+  return client.multicast(user_id_array,echoMessage);
+}
+
+function multicastAudioMessage(user_id_array: string[], originalContentUrl: string, duration: number){
+  const echoMessage = {
+    type: "audio",
+    originalContentUrl: originalContentUrl,
+    duration: duration
+  };
+  return client.multicast(user_id_array,echoMessage);
+}
+
+function multicastLocationMessage(user_id_array: string[], title: string, address: string, latitude: number, longitude: number){
+  const echoMessage = {
+    type: "location",
+    title: title,
+    address: address,
+    latitude: latitude,
+    longitude: longitude
+  };
+  return client.multicast(user_id_array, echoMessage);
+};
+
+// idについてはhttps://developers.line.biz/media/messaging-api/sticker_list.pdfを参照
+function multicastStickerMessage(user_id_array: string[], packageId: number, stickerId: number){
+  const echoMessage = {
+    type: "sticker",
+    packageId: packageId,
+    stickerId: stickerId
+  };
+  return client.multicast(user_id_array, echoMessage);
+};
+
+
+
 
 // image, video, and audioのデータが返ってくる
 function getMessageContent(message_id: string){
